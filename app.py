996e9a4ecc973b269c2a91ab434d30b755a94cff
KwonCheulJin <KwonCheulJin@users.noreply.github.com>
2021-02-22 09:57:17 +0900

    js update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import requests  # requests 라이브러리 설치 필요
-from pymongo import MongoClient
+import os
+
 from flask import Flask, render_template, jsonify
+from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbsparta  # 'dbsparta'라는 이름의 db를 사용합니다. 'dbsparta' db가 없다면 새로 만듭니다.
@@ -31,6 +32,18 @@ collect = db.lotto
 # for episode in episodes:
 #     collect.insert_one(get_request_by_episode(episode))
 app = Flask(__name__)
+
+
+@app.template_filter('autoversion')
+def autoversion_filter(filename):
+    # determining fullpath might be project specific
+    fullpath = os.path.join('some_app/', filename[1:])
+    try:
+        timestamp = str(os.path.getmtime(fullpath))
+    except OSError:
+        return filename
+    newfilename = "{0}?v={1}".format(filename, timestamp)
+    return newfilename
 
 
 ## HTML을 주는 부분
